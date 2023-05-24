9fdc827a2186fe915bef9063029c9b147c6d6f3d
Renan Alves Peregrino <126730336+R3NAN-AP@users.noreply.github.com>
2023-05-23 21:34:28 -0300

    Mudanças na classe Financeiro

--- ConsultorioMedico/src/main/java/objeto/consultoriomedico/Financeiro.java
+++ ConsultorioMedico/src/main/java/objeto/consultoriomedico/Financeiro.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class Financeiro {
     
-    public  String nomeTitular;
+ 
     public double valor;
 
     public double saldo;
@@ -18,8 +18,7 @@ public class Financeiro {
     public List<String> historicoTransacao;
     public double limiteCredito;
 
-    public Financeiro(String nomeTitular, double valor, double taxaJuro, int numConta, String tipoConta, int dtAbertura, double limiteCredito) {
-        this.nomeTitular = nomeTitular;
+    public Financeiro(double valor, double taxaJuro, int numConta, String tipoConta, int dtAbertura, double limiteCredito) {
         this.valor = valor;
         this.saldo = valor;
         this.taxaJuro = taxaJuro;
@@ -33,16 +32,16 @@ public class Financeiro {
     Financeiro() {
     }
 
-    public void depositarDinheiro() { //pagarconsulta//
+    public void pagarConsulta() { //pagarconsulta//
         Scanner dinheiro = new Scanner(System.in);
-        int deposito = dinheiro.nextInt();
-        System.out.println("Foi realizado um depósito de R$" + deposito);
+        int pagarConsult = dinheiro.nextInt();
+        System.out.println("Foi realizado um pagamento no valor de R$" + pagarConsult);
     }
 
-    public void sacarDinheiro() {//Pagarfuncionario
+    public void pagarFuncionario() {//Pagarfuncionario
         Scanner dinheiro = new Scanner(System.in);
-        int deposito = dinheiro.nextInt();
-        System.out.println("Foi realizado um saque de R$" + deposito);
+        int pagarFunc = dinheiro.nextInt();
+        System.out.println("Foi realizado um pagamento de Funcionário no valor de R$" + pagarFunc);
     }
     public void calcularJuros() {
         Scanner dinheiro = new Scanner(System.in);
@@ -53,13 +52,13 @@ public class Financeiro {
     public void historicoTransacao(){
         ArrayList<String> historico = new ArrayList<String>();
         
-        historico.add("Dia 13/01/2023 foi realizado um saque de R$ 190,00");
+        historico.add("Dia 13/01/2023 foi realizado um pagamento de uma consulta no valor de R$ 190,00");
         historico.add("---------------------------------------------------");
-        historico.add("Dia 30/03/2023 foi realizado um depósito de R$ 1000,00");
+        historico.add("Dia 30/03/2023 foi realizado um pagamento para funcionário no valor de R$ 1000,00");
         historico.add("---------------------------------------------------");
-        historico.add("Dia 14/04/2023 foi realizado um saque de R$ 190,00");
+        historico.add("Dia 14/04/2023 foi realizado um pagamento de uma consulta no valor de R$ 300,00");
         historico.add("---------------------------------------------------");
-        historico.add("Dia 20/06/2023 foi realizado um saque de R$4000,00");
+        historico.add("Dia 20/06/2023 foi realizado um pagamento de funcionário no valor de R$4000,00");
         
         for (String h: historico){
         System.out.println(historico);
@@ -71,8 +70,8 @@ public class Financeiro {
      
         System.out.println("=============================");
         System.out.println("Escolha o que deseja executar em Financeiro conforme a tabela");
-        System.out.println("1 - Depositar Dinheiro");
-        System.out.println("2 - Sacar Dinheiro");
+        System.out.println("1 - Pagar Consulta");
+        System.out.println("2 - Pagar Funcionario");
         System.out.println("3 - Juros");
         System.out.println("4 - Histórico de Transação");
         System.out.println("5 - Sair");
@@ -82,18 +81,18 @@ public class Financeiro {
         
         switch (opcao) {
             case 1 -> {
-                System.out.println("Você escolheu Depositar Dinheiro ");
-                System.out.println("Digite o valor que deseja depositar");
-                depositarDinheiro();
+                System.out.println("Você escolheu Pagar Consulta ");
+                System.out.println("Digite o valor do pagamento");
+                pagarConsulta();
             }
             case 2 -> {
-                System.out.println("Você escolheu Sacar Dinheiro;");
-                System.out.println("Digite o valor que deseja sacar");
-                sacarDinheiro();
+                System.out.println("Você escolheu Pagar Funcionario");
+                System.out.println("Digite o valor do pagamento do funiconário");
+                pagarFuncionario();
             }
             case 3 -> {
                 System.out.println("Você escolheu Juros");
-                System.out.println("O juros é de 1%");
+                System.out.println("O juros é de 1%, caso tenha vencido o prazo de pagamento");
                 System.out.println("Digite o valor que deseja consultar");
                 calcularJuros();
             }
