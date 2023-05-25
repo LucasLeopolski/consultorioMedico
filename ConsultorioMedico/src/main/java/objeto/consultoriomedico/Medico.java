6734ed3daa8f2fff6d36891bf10f0383751ba77c
LucasLeopolski <126730157+LucasLeopolski@users.noreply.github.com>
2023-05-24 22:08:40 -0300

    Update Medico.java
    Atualizando médico

--- ConsultorioMedico/src/main/java/objeto/consultoriomedico/Medico.java
+++ ConsultorioMedico/src/main/java/objeto/consultoriomedico/Medico.java
@@ -3,44 +3,43 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package objeto.consultoriomedico;
-import java.util.Scanner; 
+
+import java.util.Scanner;
 
 /**
  *
  * @author lucas
  */
-public class Medico extends Pessoa {
+public class Medico extends Funcionario {
+
     Agendamento agendamento = new Agendamento();
+    Receita receita = new Receita(" ", 10); 
     Scanner ler = new Scanner(System.in);
-    
-    
-    private int crm; 
+
+    public Medico(String nome, int idade, String cpf, String telefone, String email, String cep, String endereco) {
+        super(nome, idade, cpf, telefone, email, cep, endereco);
+
+    }
+    private int crm;
     public String especialidade;
     public String prescricao;
-    
-    public Medico(){}
+
   
-    public Medico(int crm, String especialidade, int idPessoa, String nome, String sobrenome, int idade, char sexo, String cpf, String telefone, String email, String cep, String endereco) {
-        super(idPessoa, nome, sobrenome, idade, sexo, cpf, telefone, email, cep, endereco);
-        this.crm = crm;
-        this.especialidade = especialidade;
+    public void realizarConsulta() {
+        System.out.println("O médico está realizando uma consulta no momento!");
+
     }
-    
-    public void realizarConsulta(){
-    System.out.println("O médico está realizando uma consulta no momento!");
-    
-    }
-    
-    public void prescreverReceita(){
+   
+        
+
+    /*public void prescreverReceita() {
         System.out.println("        MEDIC SOFTWARES        ");
-        System.out.println("RECEITA NÚMERO: "+agendamento.getIdAgendamento());
-        System.out.println("PACIENTE: "+agendamento.nomeCompleto);
-        System.out.println("TELEFONE: "+agendamento.getTelefone());
+        System.out.println("RECEITA NÚMERO: " + agendamento.getIdAgendamento());
+        System.out.println("PACIENTE: " + agendamento.nomeCompleto);
+        System.out.println("TELEFONE: " + agendamento.getTelefone());
         System.out.println("\n\nPRESCRIÇÃO: ");
-        prescricao = ler.nextLine();
-                
-    
+        prescricao = ler.nextLine();*/
+
     }
-    
-    
-}
+
+
