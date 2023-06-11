a5f55e336d4cf92f972411f816aa4b072886ac50
LucasLeopolski <126730157+LucasLeopolski@users.noreply.github.com>
2023-06-11 09:47:57 -0300

    Corrigindo erros

--- ConsultorioMedico/src/main/java/objeto/consultoriomedico/Medico.java
+++ ConsultorioMedico/src/main/java/objeto/consultoriomedico/Medico.java
@@ -4,6 +4,7 @@
  */
 package objeto.consultoriomedico;
 
+
 import java.util.Scanner;
 
 /**
@@ -15,13 +16,13 @@ public class Medico extends Funcionario {
     Receita receita = new Receita();
     Scanner ler = new Scanner(System.in);
 
-    public Medico(String nome, int idade, String cpf, String telefone, String email, String cep, String endereco) {
-        super(nome, idade, cpf, telefone, email, cep, endereco);
+    public Medico(String nome, int idade, String cpf, String telefone, String email, String cep, String endereco, String cargo) {
+        super(nome, idade, cpf, telefone, email, cep, endereco, cargo);
 
     }
+
     private int crm;
     public String especialidade;
-    public String prescricao;
 
     public void realizarConsulta() {
         System.out.println("O médico está realizando uma consulta no momento!");
@@ -30,6 +31,5 @@ public class Medico extends Funcionario {
         receita.emitirReceita();
 
     }
-
+        
 }
-
