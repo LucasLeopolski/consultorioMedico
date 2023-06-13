1f7e5f1107907ab95021c59555dea3387b0ac27f
Lucas Pessoli <pessolilucas@gmail.com>
2023-06-13 19:57:35 -0300

    FIX Identação do Datas

--- ConsultorioMedico/src/main/java/objeto/consultoriomedico/Datas.java
+++ ConsultorioMedico/src/main/java/objeto/consultoriomedico/Datas.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package objeto.consultoriomedico;
 
 import java.util.Calendar;
@@ -14,21 +10,21 @@ import java.util.Random;
 public class Datas {
     
         public String gerarData(boolean resposta){
-        String data;
-        if(resposta == true){ //retornará a data atual
-            Calendar calendario = Calendar.getInstance();
-            data = calendario.getTime().toString();   
-        }else{
-            Random random = new Random(); //retorna uma data randomizada
-            int dia = random.nextInt(365);
-            int mes = random.nextInt(12) + 1;
-            Calendar calendario = Calendar.getInstance();
-            calendario.set(Calendar.YEAR, 2023);
-            calendario.set(Calendar.MONTH, mes - 1);
-            calendario.set(Calendar.DAY_OF_MONTH, 1);
-            calendario.add(Calendar.DAY_OF_MONTH, dia);
-            data = calendario.getTime().toString();
+            String data;
+            if(resposta == true){ //retornará a data atual
+                Calendar calendario = Calendar.getInstance();
+                data = calendario.getTime().toString();   
+            }else{
+                Random random = new Random(); //retorna uma data randomizada
+                int dia = random.nextInt(365);
+                int mes = random.nextInt(12) + 1;
+                Calendar calendario = Calendar.getInstance();
+                calendario.set(Calendar.YEAR, 2023);
+                calendario.set(Calendar.MONTH, mes - 1);
+                calendario.set(Calendar.DAY_OF_MONTH, 1);
+                calendario.add(Calendar.DAY_OF_MONTH, dia);
+                data = calendario.getTime().toString();
+            }
+            return data;
         }
-        return data;
-    }
 }
