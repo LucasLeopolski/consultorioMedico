1b555c5f889730572565c8dbab824f6403c9f52f
LucasLeopolski <126730157+LucasLeopolski@users.noreply.github.com>
2023-06-13 21:43:09 -0300

    excluido itens refentes a classe especialidades

--- ConsultorioMedico/src/main/java/objeto/consultoriomedico/ConsultorioMedico.java
+++ ConsultorioMedico/src/main/java/objeto/consultoriomedico/ConsultorioMedico.java
@@ -1,65 +1,64 @@
 package objeto.consultoriomedico;
 
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class ConsultorioMedico {
 
     public static void main(String[] args) {
-        
+
         boolean entrarFuncionario = false;
         Receita receita = new Receita();
-        paciente Paciente = new paciente();
+        Agendamento agendamento = new Agendamento();
+        Paciente Paciente = new Paciente();
         Scanner ler = new Scanner(System.in);
         boolean sair = false;
         String usuario = "suporte";
         String senha = "suporte@";
-        
+
         System.out.println("Você é um funcionário? (y/n)");
         String resposta = ler.nextLine();
-        
-        if(resposta.equalsIgnoreCase("y")){
+
+        if (resposta.equalsIgnoreCase("y")) {
             System.out.println("Informe seu usuario: ");
             String usuarioInformado = ler.nextLine();
             System.out.println("Informe sua senha:");
             String senhaInformada = ler.nextLine();
-            if(usuarioInformado.equals(usuario) && senhaInformada.equals(senha)){
+            if (usuarioInformado.equals(usuario) && senhaInformada.equals(senha)) {
                 entrarFuncionario = true;
-            }else{
+            } else {
                 System.out.println("Senha inválida... Continuando como paciente");
             }
-        }else{
+        } else {
             System.out.println("Entrando como paciente...");
         }
         while (!sair) {
-            if(entrarFuncionario == true){
+            if (entrarFuncionario == true) {
+
                 System.out.println("\nBem vindo(a) ao Medic Softwares! No que podemos lhe ajudar!");
-                System.out.println("(1)Agendamento de consultas\n(2)Especialidades\n(3)Receitas\n(4)Paciente\n(5)Funcionario\n(6)Financeiro\n(0)Sair");
+                System.out.println("(1)Agendamento de consultas\n\n(2)Receitas\n(3)Paciente\n(4)Funcionario\n(5)Financeiro\n(0)Sair");
                 String menu = ler.nextLine();
                 switch (menu) {
                     case "1" -> {
                         System.out.println("Voce escolheu Agendamento de Consulta");
-    //                    agendamento.agendarHorario();
+                        agendamento.agendarHorario();
                     }
                     case "2" -> {
-                        System.out.println("Voce escolheu Especialidades!");
-    //                    especialidade.menu();
-                    }
-                    case "3" -> {
                         System.out.println("Voce escolheu Receitas");
                         receita.menu();
                     }
-                    case "4" -> {
+                    case "3" -> {
                         System.out.println("Voce escolheu Paciente");
                         Paciente.menu();
                     }
-                    case "5" -> {
+                    case "4" -> {
                         System.out.println("Voce escolheu Funcionario");
                         MenuFuncionario menuFuncionario = new MenuFuncionario();
                         menuFuncionario.exibirMenu();
                     }
-                    case "6" -> {
+                    case "5" -> {
                         System.out.println("Voce escolheu Financeiro");
-                        Financeiro financeiro = new Financeiro();                          
+                        Financeiro financeiro = new Financeiro();
                         financeiro.menu();
                     }
                     case "0" -> {
@@ -69,23 +68,20 @@ public class ConsultorioMedico {
                     default -> {
                         System.out.println("OPÇÃO INVALIDA!! TENTE NOVAMENTE\n\n====================================================");
                     }
-                }   
-            }else{
+                }
+            } else {
+
                 System.out.println("\nBem vindo(a) ao Medic Softwares! No que podemos lhe ajudar!");
-                System.out.println("(1)Agendamento de consultas\n(2)Especialidades\n(3)Financeiro\n(0)Sair");
+                System.out.println("(1)Agendamento de consultas\n\n(2)Financeiro\n(0)Sair");
                 String menu = ler.nextLine();
-                switch(menu){
+                switch (menu) {
                     case "1" -> {
                         System.out.println("Voce escolheu Agendamento de Consulta");
-    //                    agendamento.agendarHorario();
-                    }
-                    case "2" -> {
-                        System.out.println("Voce escolheu Especialidades!");
-    //                    especialidade.menu();
+                        agendamento.agendarHorario();
                     }
                     case "3" -> {
                         System.out.println("Voce escolheu Financeiro");
-                        Financeiro financeiro = new Financeiro();                          
+                        Financeiro financeiro = new Financeiro();
                         financeiro.pagarConsulta();
                     }
                     case "0" -> {
@@ -95,7 +91,7 @@ public class ConsultorioMedico {
                     default -> {
                         System.out.println("OPÇÃO INVALIDA!! TENTE NOVAMENTE\n\n====================================================");
                     }
-                        
+
                 }
             }
         }
