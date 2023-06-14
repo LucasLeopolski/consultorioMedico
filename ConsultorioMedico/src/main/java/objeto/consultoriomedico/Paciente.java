538627077ee53aea3cc14b6ff5151ef60a9c6112
LucasLeopolski <126730157+LucasLeopolski@users.noreply.github.com>
2023-06-14 18:32:49 -0300

    Subindo classe Uelberti

--- ConsultorioMedico/src/main/java/objeto/consultoriomedico/Paciente.java
+++ ConsultorioMedico/src/main/java/objeto/consultoriomedico/Paciente.java
@@ -12,59 +12,41 @@ import java.util.Scanner;
  */
 public class Paciente extends Pessoa {
     
-public Paciente() {
+     private int idPaciente;
+    public Scanner ler;
+
+    public Paciente() {
+    
+ ler = new Scanner(System.in);
     }
 
     public Paciente(int idPessoa, String nome, String sobrenome, int idade, char sexo, String cpf, String telefone, String email, String cep, String endereco) {
         super(idPessoa, nome, sobrenome, idade, sexo, cpf, telefone, email, cep, endereco);
+        ler = new Scanner(System.in);
     }
-    public int idPaciente;
 
     public void atualizarPaciente() {
-        Scanner ler = new Scanner(System.in);
         int opcao;
 
         do {
-            System.out.println("Qual campo você deseja atualizar?");
-            System.out.println("1 - Nome");
-            System.out.println("2 - Idade");
-            System.out.println("3 - CPF");
-            System.out.println("4 - Telefone ");
-            System.out.println("5 - E-mail");
-            System.out.println("6 - CEP");
-            System.out.println("7 - Endereço");
-            System.out.println("0 - Sair");
-
+            exibirOpcoesAtualizacao();
             opcao = ler.nextInt();
 
             switch (opcao) {
                 case 1:
-                    System.out.print("Digite o novo nome do paciente: ");
-                    this.nome = ler.next();
+                    atualizarNome();
                     break;
                 case 2:
-                    System.out.print("Digite a nova idade do paciente: ");
-                    this.idade = ler.nextInt();
+                    atualizarIdade();
                     break;
                 case 3:
-                    System.out.print("Digite o novo CPF do paciente: ");
-                    this.cpf = ler.next();
+                    atualizarCpf();
                     break;
                 case 4:
-                    System.out.print("Digite o novo telefone fixo do paciente: ");
-                    this.telefone = ler.next();
+                    atualizarTelefone();
                     break;
                 case 5:
-                    System.out.print("Digite o novo e-mail do paciente: ");
-                    this.email = ler.nextLine();
-                    break;
-                case 6:
-                    System.out.print("Digite o novo CEP do paciente: ");
-                    this.cep = ler.next();
-                    break;
-                case 7:
-                    System.out.print("Digite o novo endereço do paciente: ");
-                    this.endereco = ler.nextLine();
+                    atualizarEndereco();
                     break;
                 case 0:
                     System.out.println("Saindo do menu de atualização...");
@@ -75,50 +57,94 @@ public Paciente() {
         } while (opcao != 0);
 
         System.out.println("Dados do paciente atualizados com sucesso!");
-        menu();
+        exibirMenu();
     }
 
     public void pesquisarPaciente() {
-        Scanner ler = new Scanner(System.in);
-
         System.out.print("Digite o CPF do paciente que deseja pesquisar: ");
         String cpfPesquisa = ler.nextLine();
 
         if (cpfPesquisa.equals(this.cpf)) {
-            System.out.println("Paciente encontrado!");
-            System.out.println("Nome: " + this.nome);
-            System.out.println("Idade: " + this.idade);
-            System.out.println("CPF: " + this.cpf);
-            System.out.println("Telefone fixo: " + this.telefone);
-            System.out.println("E-mail: " + this.email);
-            System.out.println("CEP: " + this.cep);
-            System.out.println("Endereço: " + this.endereco);
+            exibirInformacoesPaciente();
         } else {
             System.out.println("Paciente não encontrado!");
         }
-        menu();
+        exibirMenu();
+    }
+
+    private void exibirInformacoesPaciente() {
+        System.out.println("Paciente encontrado!");
+        System.out.println("Nome: " + this.nome);
+        System.out.println("Sobrenome:  " + this.sobrenome);
+        System.out.println("Idade: " + this.idade);
+        System.out.println("CPF: " + this.cpf);
+        System.out.println("Telefone " + this.telefone);
+        System.out.println("Endereço: " + this.endereco);
     }
 
     @Override
     public void cadastrarPaciente() {
-        System.out.println("Nome: " + this.nome);
-        System.out.println("Sobrenome: " + this.sobrenome);
-        System.out.println("Telefone: " + this.telefone);
-        System.out.println("Cpf: " + this.cpf);
-        System.out.println("Endereço: " + this.endereco);
-        System.out.printf("Id Paciente: ");
-       // this.novoPaciente.idPaciente = random.nextInt(1000);
-        //System.out.println(novoPaciente.idPaciente);
-        menu();
+        ler = new Scanner(System.in);
+        System.out.println("Informe seu nome: ");
+        nome = ler.nextLine();
+        System.out.println("Informe seu sobrenome: ");
+        sobrenome = ler.nextLine();
+        System.out.println("Informe seu idade: ");
+        idade = ler.nextInt();
+        System.out.println("Informe seu telefone: ");
+        telefone = ler.nextLine();
+        System.out.println("Informe seu CPF: ");
+        cpf = ler.nextLine();
+        System.out.println("Informe seu endereço: ");
+        endereco = ler.nextLine();
+        System.out.println("Paciente cadastrado com sucesso!");
+        exibirMenu();
     }
 
-    public void menu() {
-        Scanner ler = new Scanner(System.in);
+    private void exibirOpcoesAtualizacao() {
+        System.out.println("Qual campo você deseja atualizar?");
+        System.out.println("1 - Nome");
+        System.out.println("2 - Idade");
+        System.out.println("3 - CPF");
+        System.out.println("4 - Telefone");
+        System.out.println("7 - Endereço");
+        System.out.println("0 - Sair");
+    }
 
+    private void atualizarNome() {
+        System.out.print("Digite o novo nome do paciente: ");
+        this.nome = ler.next();
+    }
+
+    private void atualizarIdade() {
+        System.out.print("Digite a nova idade do paciente: ");
+        this.idade = ler.nextInt();
+    }
+
+    private void atualizarCpf() {
+        System.out.print("Digite o novo CPF do paciente: ");
+        this.cpf = ler.next();
+    }
+
+    private void atualizarTelefone() {
+        System.out.print("Digite o novo telefone do paciente: ");
+        this.telefone = ler.nextLine();
+    }
+
+    private void atualizarEndereco() {
+        System.out.print("Digite o novo endereço do paciente: ");
+        ler.nextLine(); // Limpa o buffer do scanner antes de ler a próxima linha
+        this.endereco = ler.nextLine();
+    }
+
+    public void exibirMenu() {
         for (int i = 0; i < 3; i++) {
             String escolha;
             System.out.println("Escolha uma opção!");
-            System.out.println("(0) Cadastrar Paciente\n(1) Atualizar Paciente\n(2) Pesquisar Paciente\n(3) Menu Principal");
+            System.out.println("(0) Cadastrar Paciente");
+            System.out.println("(1) Atualizar Paciente");
+            System.out.println("(2) Pesquisar Paciente");
+            System.out.println("(3) Menu Principal");
             escolha = ler.next();
             switch (escolha) {
                 case "0" ->
